c4ca54148b01b8e8ddc2c3fccf09bd8b2224e711
Anthonio Chen <wacxr123@sina.com>
2025-01-22 21:10:13 +0800

    cuda change

--- self-refine_generator.py
+++ self-refine_generator.py
@@ -9,8 +9,8 @@ import jsonlines
 from tqdm import tqdm
 from itertools import islice
 
-device = "cuda:0"
-verifier_device = "cuda:1"
+device = "cuda:2"
+verifier_device = "cuda:3"
 max_new_tokens = 512
 verifier_max_new_tokens = 256
 model_path = "meta-llama/Llama-3.1-8B-Instruct"
